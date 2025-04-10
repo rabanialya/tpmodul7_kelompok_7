1bf7fc4934e1f6f68b4c0c0d21ffc4c3f1fb87c8
alya <alyarabani33@gmail.com>
2025-04-10 13:29:17 +0700

    menambahkan class KuliahMahasiswa2311104076

--- tpmodul7_kelompok_07/tpmodul7_kelompok_07/Program.cs
+++ tpmodul7_kelompok_07/tpmodul7_kelompok_07/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class KuliahMahasiswa2311104076
+{
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class CourseList
+    {
+        public List<Course> courses { get; set; }
+    }
+
+    public void ReadJSON()
+    {
+        string filePath = "tp7_2_2311104076.json";
+        string jsonText = File.ReadAllText(filePath);
+
+        CourseList data = JsonSerializer.Deserialize<CourseList>(jsonText);
+
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        int i = 1;
+        foreach (var course in data.courses)
+        {
+            Console.WriteLine($"MK {i} {course.code} - {course.name}");
+            i++;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KuliahMahasiswa2311104076 kuliah = new KuliahMahasiswa2311104076();
+        kuliah.ReadJSON();
+    }
+}
