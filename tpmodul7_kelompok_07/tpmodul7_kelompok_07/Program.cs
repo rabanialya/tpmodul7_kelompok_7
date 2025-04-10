8e4155a62795022427b9d1da132d62c588d1bb97
zclimaxsp <dhiemasikhsan@gmail.com>
2025-04-10 12:38:52 +0700

    menambahkan class DataMahasiswa2311104046

--- tpmodul7_kelompok_07/tpmodul7_kelompok_07/Program.cs
+++ tpmodul7_kelompok_07/tpmodul7_kelompok_07/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class DataMahasiswa2311104046
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public class Mahasiswa
+    {
+        public Nama nama { get; set; }
+        public long nim { get; set; }
+        public string fakultas { get; set; }
+    }
+
+    public void ReadJSON()
+    {
+        string json = File.ReadAllText("tp7_1_2311104046.json");
+        Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(json);
+        Console.WriteLine($"Nama {mhs.nama.depan} {mhs.nama.belakang} dengan nim {mhs.nim} dari fakultas {mhs.fakultas}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa2311104046 data = new DataMahasiswa2311104046();
+        data.ReadJSON();
+    }
+}
